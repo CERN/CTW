34eec08272e418d55d0a72b1df4d4433704975fd
Ralf Erik Rossel <Ralf.Rossel@gmail.com>
2019-01-26 18:14:54 +0100

    Added crafts to cook papers from ideas by burning references.

--- mods/gjcctw/init.lua
+++ mods/gjcctw/init.lua
@@ -3,6 +3,9 @@ print("Found the GameJam CERN Craft The Web mod! Yay!")
 local ret = dofile(minetest.get_modpath("gjcctw") .. "/script.lua")
 print(ret)
 
+
+-- Registering basic building blocks to craft the web
+
 minetest.register_node("gjcctw:reference", {
     description = "This is a reference node",
     tiles = {"reference_16x16.png"},
@@ -19,4 +22,20 @@ minetest.register_node("gjcctw:paper", {
     description = "This is a paper node",
     tiles = {"paper_16x16.png"},
     groups = {cracky=1, oddly_breakable_by_hand=2}
+})
+
+
+-- Registering crafts to cook a paper from ideas by burning references.
+
+minetest.register_craft({
+    type = "cooking",
+    output = "gjcctw:paper",
+    recipe = "gjcctw:idea",
+    cooktime = 10,
+})
+
+minetest.register_craft({
+    type = "fuel",
+    recipe = "gjcctw:reference",
+    burntime = 30,
 })
