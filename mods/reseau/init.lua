06b97477bb783871714208b14dfb69a69344c031
Jeija <norrepli@gmail.com>
2019-07-06 16:31:12 +0200

    Networking: Tape delivery to gain team discovery points

--- mods/reseau/init.lua
+++ mods/reseau/init.lua
@@ -1,6 +1,9 @@
 reseau = {}
 
-reseau.era_speed = 10
+reseau.era = {}
+reseau.era.genspeed = 10 -- experiment data generation speed in MB/s
+reseau.era.tape_capacity = 500 -- tape capacity in MB
+reseau.era.dp_multiplier = 1 -- discovery points per delivered MB
 
 dofile(minetest.get_modpath("reseau").."/util.lua")
 dofile(minetest.get_modpath("reseau").."/technology.lua")
@@ -11,19 +14,21 @@ dofile(minetest.get_modpath("reseau").."/transmit.lua")
 dofile(minetest.get_modpath("reseau").."/modstorage.lua")
 dofile(minetest.get_modpath("reseau").."/transmittermgmt.lua")
 
+
+local TX_INTERVAL = 3
 local transmitter_get_formspec = function(meta)
 	local cache = meta:get_int("cache")
 
 	return "size[8,5;]"..
-		"list[context;tapes;3.5,0;8,4;]"..
-		"label[0,1.5;Experiments generate tapes, transport them to the computing center]"..
-		"label[0,1.9;Data generation speed: "..reseau.era_speed.." MB/s]"..
-		"label[0,2.3;Cached data: "..cache.." MB]"..
+		"list[context;tapes;2,0;4,1;]"..
+		"label[0,1.5;Experiments generate data that has to be moved to the computing center.]"..
+		"label[0,1.9;Data can be transported manually by carrying tapes or by a network link.]"..
+		"label[0,2.3;Data generation speed: "..reseau.era.genspeed.." MB/s]"..
+		"label[0,2.7;Cached data: "..cache.." MB / Tape capacity: "..reseau.era.tape_capacity.." MB]"..
 		"list[current_player;main;0,4;8,1;]"
 end
 
-local TX_INTERVAL = 3
-local TAPE_CAPACITY = 10
+-- TODO: team ownership (save in tape meta)!
 minetest.register_node(":reseau:testtransmitter", {
 	description = "Transmitter (Testing)",
 	tiles = {"default_tree.png"},
@@ -37,15 +42,23 @@ minetest.register_node(":reseau:testtransmitter", {
 			autotransmit = {
 				interval = TX_INTERVAL,
 				action = function(pos)
-					local meta = minetest.get_meta(pos)
-					local inv = meta:get_inventory()
+					-- try to transmit data via network, otherwise store on tape
 					if (not reseau.transmit_first(pos, "hello world!")) then
+						local meta = minetest.get_meta(pos)
 						local cache = meta:get_int("cache") or 0
 
-						cache = cache + reseau.era_speed / TX_INTERVAL
-						if cache > TAPE_CAPACITY then
-							cache = cache - TAPE_CAPACITY
-							inv:add_item("tapes", "reseau:tape")
+						cache = cache + reseau.era.genspeed * TX_INTERVAL
+						if cache > reseau.era.tape_capacity then
+							cache = cache - reseau.era.tape_capacity
+
+							local inv = meta:get_inventory()
+							local tape_stack = ItemStack("reseau:tape 1")
+							local tape_meta = tape_stack:get_meta()
+							tape_meta:set_int("capacity", reseau.era.tape_capacity)
+							tape_meta:set_string("team", "red")
+							tape_meta:set_string("description", reseau.era.tape_capacity.." MB tape (team red)")
+
+							inv:add_item("tapes", tape_stack)
 						end
 						meta:set_int("cache", cache)
 
@@ -58,14 +71,20 @@ minetest.register_node(":reseau:testtransmitter", {
 	on_construct = function(pos)
 		local meta = minetest.get_meta(pos)
 		local inv = meta:get_inventory()
-		inv:set_size("tapes", 1)
+		inv:set_size("tapes", 4)
 
 		meta:set_string("formspec", transmitter_get_formspec(meta))
 	end
 })
 
-local receiveCount = 0
-local receiveCountHud = nil
+local receiver_get_formspec = function(meta)
+	return "size[8,5;]"..
+		"list[context;tapes;3.5,0;1,1;]"..
+		"label[0,1.5;The computing center processes and stores experiment data to make discoveries.]"..
+		"label[0,1.9;Place tapes here to gain discovery points!]"..
+		"list[current_player;main;0,4;8,1;]"
+end
+
 minetest.register_node(":reseau:testreceiver", {
 	description = "Receiver (Testing)",
 	tiles = {"default_water.png"},
@@ -77,25 +96,35 @@ minetest.register_node(":reseau:testreceiver", {
 			},
 			rules = reseau.rules.default,
 			action = function(pos, content)
-				receiveCount = receiveCount + 1
-				local player = minetest.get_player_by_name("singleplayer")
-				local hudtext = "Received: " .. receiveCount
-
-				if receiveCountHud ~= nil then
-					player:hud_change(receiveCountHud, "text", hudtext)
-				else
-					receiveCountHud = player:hud_add({
-						hud_elem_type = "text",
-						position = {x = 1.0, y = 0.0},
-						offset = {x = -100, y = 20},
-						text = hudtext,
-						alignment = {x = 0, y = 0},
-						scale = {x = 100, y = 100}
-					})
-				end
 			end
 		}
 	},
+	on_metadata_inventory_put = function(pos, listname, index, stack, player)
+		if stack:get_name() == "reseau:tape" then
+			local tape_meta = stack:get_meta()
+			local capacity = tape_meta:get_int("capacity")
+			local team = tape_meta:get_string("team")
+			local dp = capacity * reseau.era.dp_multiplier
+
+			if teams.get(team) then
+				teams.add_points(team, dp)
+				local chatmsg = player:get_player_name() .. " delivered "..capacity..
+					" MB to the computing center, generating "..dp.." discovery points!"
+				teams.chat_send_team(team, minetest.colorize("#50ff50", chatmsg))
+			end
+
+			local meta = minetest.get_meta(pos)
+			local inv = meta:get_inventory()
+			inv:remove_item("tapes", "reseau:tape")
+		end
+	end,
+	on_construct = function(pos)
+		local meta = minetest.get_meta(pos)
+		local inv = meta:get_inventory()
+		inv:set_size("tapes", 1)
+
+		meta:set_string("formspec", receiver_get_formspec(meta))
+	end
 })
 
 local ROUTER_DELAY = 3
@@ -156,6 +185,6 @@ minetest.register_node(":reseau:testrouter", {
 
 minetest.register_craftitem(":reseau:tape", {
 	image = "reseau_tape.png",
-	stack_max = 4,
-    	description="Data Tape"
+	stack_max = 1,
+	description="Data Tape"
 })
