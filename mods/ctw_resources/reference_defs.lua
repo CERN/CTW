2b068589aec71a98c79e942b268987db3fb41c83
Ekdohibs <nathanael.courant@laposte.net>
2019-07-06 11:15:21 +0200

    Move book definitions from ctw_resources to books

--- mods/ctw_resources/reference_defs.lua
+++ mods/ctw_resources/reference_defs.lua
@@ -1,17 +1,1 @@
-
-ctw_resources.register_reference("ctw_resources:red_book", {
-        description = "Red Book",
-        _ctw_longdesc = "This is a book that is red and contains interesting information",
-        inventory_image = "ctw_resources_red_book.png",
-})
-ctw_resources.register_reference("ctw_resources:blue_book", {
-        description = "Blue Book",
-        _ctw_longdesc = "This is a book that is blue and contains interesting information",
-        inventory_image = "ctw_resources_blue_book.png",
-})
-ctw_resources.register_reference("ctw_resources:green_book", {
-        description = "Green Book",
-        _ctw_longdesc = "This is a book that is green and contains interesting information",
-        inventory_image = "ctw_resources_green_book.png",
-})
-
+-- Nothing here anymore: moved to the books mod
