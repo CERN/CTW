2cc5408c82febab7d6b0aa08f769638cbd7fe4a7
Jeija <norrepli@gmail.com>
2019-07-08 11:20:45 +0200

    Networking: Notify players when entering new era

--- mods/reseau/init.lua
+++ mods/reseau/init.lua
@@ -24,6 +24,7 @@ local MAX_HOP_COUNT = 50
 -- #       Eras         #
 -- ######################
 reseau.era.register(true, 1986, {
+	name = "internet stone age",
 	tape_capacity = 500,
 	dp_multiplier = 1,
 	experiment_throughput_limit = 10,
@@ -32,6 +33,7 @@ reseau.era.register(true, 1986, {
 })
 
 reseau.era.register(1986, 1990, {
+	name = "after-chernobyl",
 	tape_capacity = 500,
 	dp_multiplier = 1,
 	experiment_throughput_limit = 10,
@@ -40,6 +42,7 @@ reseau.era.register(1986, 1990, {
 })
 
 reseau.era.register(1990, 1994, {
+	name = "early nineties",
 	tape_capacity = 500,
 	dp_multiplier = 1,
 	experiment_throughput_limit = 10,
@@ -48,6 +51,7 @@ reseau.era.register(1990, 1994, {
 })
 
 reseau.era.register(1994, true, {
+	name = "late nineties",
 	tape_capacity = 500,
 	dp_multiplier = 1,
 	experiment_throughput_limit = 10,
