3d949716169316b7ad93521fccdc4bd9a06592b9
SmallJoker <mk939@ymail.com>
2019-07-08 11:24:56 +0200

    Add translations.  replacements need fixing!

--- mods/npc/dialogue_tree.lua
+++ mods/npc/dialogue_tree.lua
@@ -1,14 +1,16 @@
 
+local S = minetest.get_translator("npc")
+
 -- == THE PROGRAMMER == --
 
 npc.register_npc("programmer", {
-	infotext = "Computer specialist",
+	infotext = S("Computer specialist"),
 	textures = { "npc_skin_geopbyte.png" }
 })
 
 npc.register_event("programmer", {
 	id = "test1",
-	dialogue = "$PLAYER, line1\n\n\nlineX;:/\\]",
+	dialogue = S("$PLAYER, line1\n\n\nlineX;:/\\]"),
 	options = {
 		{
 			text = "get",
@@ -24,21 +26,21 @@ npc.register_event("programmer", {
 })
 
 npc.register_event_idea_discover("programmer", "ascii", {
-	dialogue = "What if there was a standard for character encoding?"
+	dialogue = S("What if there was a standard for character encoding?")
 })
 
 npc.register_event_idea_approve("programmer", "ascii")
 
 npc.register_npc("engineer", {
-	infotext = "Engineer",
+	infotext = S("Engineer"),
 	textures = { "npc_skin_doctor_sam.png" }
 })
 
 npc.register_event("engineer", {
-	dialogue = "Hello $PLAYER. Good luck on your mission! Move tapes to gain DPs.",
+	dialogue = S("Hello $PLAYER. Good luck on your mission! Move tapes to gain DPs."),
 })
 
 npc.register_event_idea_discover("engineer", "ethernet", {
 	dp_min = 2000,
-	dialogue = "Some kind of twisted cable thingy would be lovely."
+	dialogue = S("Some kind of twisted cable thingy would be lovely.")
 })
