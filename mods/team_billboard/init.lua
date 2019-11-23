CERN/CTW

commit a8859adefc9fee54ceca122fc200fc3cb70fd4db

Game balancing...
Add ctw_resources.compare_all_ideas
Add DP penalty when teams drift apart
Cheatengine: Update year

mods/team_billboard/init.lua
@@ -90,10 +90,9 @@ function team_billboard.show_billboard_form(pname, tname, pers_msg)
 			local istate = ctw_resources.get_team_idea_state(idea_id, team)
 			table.insert(form, "button[2,"..(1.5 + index)..";4,1;idea_"..idea_id..";"..idea.name.."]")
 			if istate.state == "inventing" then
-				local total = idea.invention_dp
-				local have = istate.target
-				local percent = math.floor(100 * (have/total))
-				table.insert(form, "label[7,"..(1.5 + index)..";Prototyping "..percent.."% ("..have.."/"..total.." DP)]")
+				-- Researching speed may change
+				local percent = math.floor(100 * idea.have / idea.invention_dp)
+				table.insert(form, "label[7,"..(1.5 + index)..";Prototyping "..percent.."%]")
 			else
 				table.insert(form, "label[7,"..(1.5 + index)..";State: "..istate.state.."]")
 			end

mods/cheatengine/init.lua
@@ -38,13 +38,13 @@ local cmd_defs = {
 --BEGIN COMMAND DEFINITIONS
 books = function(name, params)
 	local player = get_player_or_nil(name, params[1])
-	local idea_id = params[2]:lower()
+	local idea_id = (params[2] or "?nil"):lower()
 	if not player then
 		return false, "Unknown player: " .. params[1]
 	end
 	local idea = ctw_resources.get_idea_raw(idea_id)
 	if not idea then
-		return false, "Unknown idea: " .. (params[2] or "?nil")
+		return false, "Unknown idea: " .. idea_id
 	end
 
 	local inv = player:get_inventory()
@@ -281,6 +281,7 @@ year = function(name, params)
 			tech_def.year >= dst_year and "undiscovered" or "gained")
 		n_found = n_found + 1
 	end
+	year.set(dst_year, team.name)
 	return true, "Changed to year " .. dst_year .. ". Found " ..
 		n_found .. " ideas and techs."
 end,

mods/ctw_resources/ideas.lua
@@ -351,8 +351,9 @@ function ctw_resources.set_team_idea_state(idea_id, team, state, param)
 end
 
 -- compare_idea(team, "eq", "discovered")
-function ctw_resources.compare_idea(idea_id, team, cmp, value)
-	local idea_state = ctw_resources.get_team_idea_state(idea_id, team).state
+function ctw_resources.compare_idea(idea, team, cmp, value)
+	local idea_state = type(idea) == "table" and idea.state or
+		ctw_resources.get_team_idea_state(idea, team).state
 
 	if cmp == "eq" then
 		return idea_state == value
@@ -369,4 +370,30 @@ function ctw_resources.compare_idea(idea_id, team, cmp, value)
 		return index_1 > index_2
 	end
 	error("Invalid comparison: " .. cmp)
+end
+
+function ctw_resources.compare_all_ideas(team, cmp, value)
+	local index_2 = table_index(ctw_resources.idea_states, value)
+	assert(index_2, "Invalid idea state 2 '" .. tostring(value) .. "'")
+
+	local ideas = {}
+	local count = 0
+	for idea_id, istate in pairs(team._ctw_resources_idea_state or {}) do
+		local index_1 = table_index(ctw_resources.idea_states, istate.state)
+		local ok = false
+		if cmp == "eq" then
+			ok = index_1 == index_2
+		elseif cmp == "lt" then
+			ok = index_1 < index_2
+		elseif cmp == "gt" then
+			ok = index_2 > index_2
+		else
+			error("Invalid comparison: " .. cmp)
+		end
+		if ok then
+			ideas[idea_id] = istate
+			count = count + 1
+		end
+	end
+	return ideas, count
 end

mods/ctw_resources/inventing.lua
@@ -45,16 +45,16 @@ function ctw_resources.start_inventing(istack, team, pname, try)
 end
 
 -- Returns the state of all ideas a team is inventing, in this format:
--- [idea_id] = { progress = <in percent>, dp = <dp accumulated so far>, dp_total = <dp total required>}
+-- [idea_id] = { dp = <dp accumulated so far>, dp_total = <dp total required>}
 function ctw_resources.get_inventing_progress(team)
+	local ideas = ctw_resources.compare_all_ideas(team, "eq", "inventing")
 	local prog = {}
-	if team._ctw_resources_idea_state then
-		for idea_id, istate in pairs(team._ctw_resources_idea_state) do
-			local idea = ctw_resources.get_idea(idea_id)
-			if istate.state == "inventing" then
-				prog[idea_id] = {dp = istate.target, dp_total = idea.invention_dp}
-			end
-		end
+	for idea_id, istate in pairs(ideas) do
+		local idea = ctw_resources.get_idea(idea_id)
+		prog[idea_id] = {
+			dp = istate.target,
+			dp_total = idea.invention_dp
+		}
 	end
 	return prog
 end
@@ -74,12 +74,23 @@ function ctw_resources.register_on_inventing_complete(func)
 end
 
 local function advance_inv_progress(ideas, delta_dp, team)
-
 	if #ideas == 0 then
 		return
 	end
 
-	local dp_share = delta_dp / #ideas
+	-- Game balancing happens here
+	-- If other teams are left behind, research gets very hard
+	local _, progress = ctw_resources.compare_all_ideas(team, "lt", "inventing")
+	local penalty = 10 -- Less impact
+	for _, team2 in pairs(teams.get_all()) do
+		if team2.name ~= team.name then
+			local _, progress2 = ctw_resources.compare_all_ideas(
+				team2, "lt", "inventing")
+			-- More impact the further they are left behind
+			penalty = penalty + math.max(0, progress - progress2)
+		end
+	end
+	local dp_share = delta_dp * 10 / penalty
 
 	for _, idea_id in ipairs(ideas) do
 		local idea = ctw_resources.get_idea(idea_id)
@@ -89,7 +100,8 @@ local function advance_inv_progress(ideas, delta_dp, team)
 
 		if istate.target >= idea.invention_dp then
 			-- The technology is invented.
-			teams.chat_send_team(team.name, "Your scientists have successfully prototyped \""..idea.name.."\"!")
+			teams.chat_send_team(team.name, "Your scientists have successfully prototyped \""..
+				idea.name.."\"!")
 			for _, tech_id in ipairs(idea.technologies_gained) do
 				ctw_technologies.gain_technology(tech_id, team)
 			end
@@ -107,6 +119,7 @@ end
 
 local prev_dp_by_team = {}
 
+-- Regularly check for DP changes and update the invention status
 local timer = 3
 
 minetest.register_globalstep(function(dtime)
@@ -116,22 +129,17 @@ minetest.register_globalstep(function(dtime)
 	timer = 2
 
 	for _,team in ipairs(teams.get_all()) do
-		if not prev_dp_by_team[team.name] then
+		local dp_delta = team.points - (prev_dp_by_team[team.name] or 0)
+		if dp_delta > 0 then
 			prev_dp_by_team[team.name] = team.points
-		else
-			local dp_delta = team.points - prev_dp_by_team[team.name]
-			if dp_delta ~= 0 then
-				prev_dp_by_team[team.name] = team.points
-				local ideas_inventing = {}
-				if team._ctw_resources_idea_state then
-					for idea_id, istate in pairs(team._ctw_resources_idea_state) do
-						if istate.state == "inventing" then
-							table.insert(ideas_inventing, idea_id)
-						end
-					end
+			local ideas_inventing = {}
+			for idea_id, istate in pairs(team._ctw_resources_idea_state or {}) do
+				if istate.state == "inventing" then
+					table.insert(ideas_inventing, idea_id)
 				end
-				advance_inv_progress(ideas_inventing, dp_delta, team)
 			end
+			-- Update stepwise the progress
+			advance_inv_progress(ideas_inventing, dp_delta, team)
 		end
 	end
 end)

commit 2c24c53b373ebe3d89bd030aa66727ef90317400

team_billboard: Fix crash introduced in a8859ad

mods/team_billboard/init.lua
@@ -91,7 +91,7 @@ function team_billboard.show_billboard_form(pname, tname, pers_msg)
 			table.insert(form, "button[2,"..(1.5 + index)..";4,1;idea_"..idea_id..";"..idea.name.."]")
 			if istate.state == "inventing" then
 				-- Researching speed may change
-				local percent = math.floor(100 * idea.have / idea.invention_dp)
+				local percent = math.floor(100 * istate.target / idea.invention_dp)
 				table.insert(form, "label[7,"..(1.5 + index)..";Prototyping "..percent.."%]")
 			else
 				table.insert(form, "label[7,"..(1.5 + index)..";State: "..istate.state.."]")