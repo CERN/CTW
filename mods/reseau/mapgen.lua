1b8e850bb5a9d55bdfaad3fcdfa0bc7377336e4f
rubenwardy <rw@rubenwardy.com>
2019-11-24 10:48:34 +0000

    Add teamisation of receivers

--- mods/reseau/mapgen.lua
+++ mods/reseau/mapgen.lua
@@ -1,6 +1,4 @@
-local names = {}
 for tname, _ in pairs(teams.get_dict()) do	
-	names[#names + 1] = "reseau:receiver_" .. tname
 	minetest.register_node("reseau:receiver_" .. tname, {
 		description = tname .. " receiver",
 		tiles = {
@@ -18,12 +16,9 @@ end
 minetest.register_lbm({
 	label = "Assign teams to things",
 	name = "reseau:teamise",
-	-- nodenames = { "group:placeholder_receiver" },
-	nodenames = names,
+	nodenames = { "group:placeholder_receiver" },
 	run_at_every_load = true,
 	action = function(pos, node)	
-		minetest.log("error", ("Found receiver at %s with name %s"):format(minetest.pos_to_string(pos), node.name))
-
 		local tname = node.name:match("_([a-z]+)$")
 
 		node.name = "reseau:receiverbase"
@@ -32,8 +27,7 @@ minetest.register_lbm({
 
 		pos.y = pos.y + 1
 		node.name = "reseau:receiverscreen"
-		minetest.swap_node(pos, node)
+		minetest.set_node(pos, node)
 		set_team(pos, tname)
-	
 	end,
 })
